740a924d2f9b24e98c44bebc17125d00021bc672
Kevin K <kbknapp@gmail.com>
2020-10-22 11:13:36 -0400

    adds standard notes

--- src/common/standard_notes.sh
+++ src/common/standard_notes.sh
@@ -1,1 +1,13 @@
-#wget https://github.com/standardnotes/desktop/releases/download/v3.4.2/standard-notes-3.4.2-linux-x86_64.AppImage
+#!/bin/bash
+
+function f_standardnotes_main() {
+    f_check_prog "wget"
+
+    f_out "Setting up standardnotes"
+
+    # Download
+    cd ~/.local/bin/ || exit
+    wget -c https://github.com/$(wget -q https://github.com/standardnotes/desktop/releases -O - | grep "standard-notes-.*-x86_64.AppImage" | head -n 1 | cut -d '"' -f 2)
+    chmod +x standard-notes-*.AppImage
+    cd || exit
+}
