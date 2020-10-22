719e1d9248658c95ab47e2774b78e44fca67fa63
Kevin K <kbknapp@gmail.com>
2020-10-22 11:13:21 -0400

    adds obsidian

--- src/common/obsidian.sh
+++ src/common/obsidian.sh
@@ -1,1 +1,13 @@
-#wget https://github.com/obsidianmd/obsidian-releases/releases/download/v0.8.9/Obsidian-0.8.9.AppImage
+#!/bin/bash
+
+function f_obsidian_main() {
+    f_check_prog "wget"
+
+    f_out "Setting up Obsidian"
+
+    # Download
+    cd ~/.local/bin/ || exit
+    wget -c https://github.com/$(wget -q https://github.com/obsidianmd/obsidian-releases/releases -O - | grep "Obsidian-.*.AppImage" | head -n 1 | cut -d '"' -f 2)
+    chmod +x Obisian-*.AppImage
+    cd || exit
+}
