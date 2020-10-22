a3dce9e2a5187e14067753cb7581426e95ba1b06
Kevin K <kbknapp@gmail.com>
2020-10-22 11:12:17 -0400

    adds appimaged

--- src/common/appimaged.sh
+++ src/common/appimaged.sh
@@ -1,4 +1,18 @@
-#wget https://github.com/AppImage/appimaged/releases/download/continuous/appimaged-x86_64.AppImage
-#chmod +x appimaged-x86_64.AppImage
-#./appimaged-x86_64.AppImage --install
+#!/bin/bash
 
+function f_appimaged_main() {
+    f_check_prog "wget"
+
+    f_out "Setting up appimaged"
+
+    # Download
+    cd ~/.local/bin/ || exit
+    wget -c https://github.com/$(wget -q https://github.com/probonopd/go-appimage/releases -O - | grep "appimaged-.*-x86_64.AppImage" | head -n 1 | cut -d '"' -f 2)
+    chmod +x appimaged-*.AppImage
+    cd || exit
+
+    # @TODO Get actual name instead of glob to prevent running multiple copies
+    # Launch
+    ./appimaged-*.AppImage
+
+}
