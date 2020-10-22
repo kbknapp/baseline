16af925e3a340422e2183a120daedd6b1b2c4f1a
Kevin K <kbknapp@gmail.com>
2020-10-22 11:12:51 -0400

    adds firefox tab hiding

--- src/common/firefox_tabs.sh
+++ src/common/firefox_tabs.sh
@@ -1,22 +1,30 @@
-#cd ~/.mozilla/firefox/*.default-release/
-#mkdir chrome
-#cat << EOF > chrome/userChrome.css
-##titlebar {
-#    visibility: collapse;
-#}
-#
-##TabsToolbar .titlebar-buttonbox-container {
-#    display: block;
-#    position: absolute;
-#    top: 2px;
-#    right: 0;
-#}
-#
-##nav-bar {
-#    padding-right: 160px
-#}
-#
-##tabbrowser-tabs {
-#  visibility: collapse !important;
-#}
-#EOF
+#!/bin/bash
+
+function f_firefoxtabs_main() {
+    f_out "Setting up Hidden Tabs in Firefox"
+
+    cd ~/.mozilla/firefox/*.default-release/ || exit
+    mkdir chrome
+    cat << EOF > chrome/userChrome.css
+#titlebar {
+    visibility: collapse;
+}
+
+#TabsToolbar .titlebar-buttonbox-container {
+    display: block;
+    position: absolute;
+    top: 2px;
+    right: 0;
+}
+
+#nav-bar {
+    padding-right: 160px
+}
+
+#tabbrowser-tabs {
+  visibility: collapse !important;
+}
+EOF
+
+    cd || exit
+}
