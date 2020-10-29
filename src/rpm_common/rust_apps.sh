f9c1910addf8813e06d8abe3272875d84d22d1a7
Kevin K <kbknapp@gmail.com>
2020-10-29 10:42:49 -0400

    adds missing dep for some cargo subcommands

--- src/rpm_common/rust_apps.sh
+++ src/rpm_common/rust_apps.sh
@@ -3,7 +3,7 @@
 function f_rust_apps_main() {
     f_out "Installing Rust applications deps"
 
-    $INSTALL_CMD openssl-devel cmake pkg-config
+    $INSTALL_CMD openssl-devel cmake pkg-config perl-FindBin
 
     f_rust_apps_common
 }
