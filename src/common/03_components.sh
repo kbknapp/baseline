9da1b8c35f0bfd5eb61ca8cb627bf82652058604
Kevin K <kbknapp@gmail.com>
2020-10-29 10:41:50 -0400

    temporarily disables firefox tabs
    We need to start firefox before it creates the default profile dir.

--- src/common/03_components.sh
+++ src/common/03_components.sh
@@ -40,7 +40,7 @@ function f_get_common_components() {
        appimaged "AppImage Integration Daemon" on \
        obsidian "Obsidian.md" off \
        standardnotes "StandardNotes" on \
-       firefoxtabs "Hide Firefox Tabs" on \
+       #firefoxtabs "Hide Firefox Tabs" on \
         --output-fd 1)
     clear
 
--- src/common/firefox_tabs.sh
+++ src/common/firefox_tabs.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 
-function f_firefoxtabs_main() {
-    f_out "Setting up Hidden Tabs in Firefox"
-
-    cd ~/.mozilla/firefox/*.default-release/ || exit
-    mkdir chrome
-    cat << EOF > chrome/userChrome.css
-#titlebar {
-    visibility: collapse;
-}
-
-#TabsToolbar .titlebar-buttonbox-container {
-    display: block;
-    position: absolute;
-    top: 2px;
-    right: 0;
-}
-
-#nav-bar {
-    padding-right: 160px
-}
-
-#tabbrowser-tabs {
-  visibility: collapse !important;
-}
-EOF
-
-    cd || exit
-}
+# function f_firefoxtabs_main() {
+#     f_out "Setting up Hidden Tabs in Firefox"
+#
+#     cd ~/.mozilla/firefox/*.default-release/ || return
+#     mkdir chrome
+#     cat << EOF > chrome/userChrome.css
+# #titlebar {
+#     visibility: collapse;
+# }
+#
+# #TabsToolbar .titlebar-buttonbox-container {
+#     display: block;
+#     position: absolute;
+#     top: 2px;
+#     right: 0;
+# }
+#
+# #nav-bar {
+#     padding-right: 160px
+# }
+#
+# #tabbrowser-tabs {
+#   visibility: collapse !important;
+# }
+# EOF
+#
+#     cd || exit
+# }
