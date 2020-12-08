00460488f078a21b24a3fe4d5303c1cfa64872a3
Kevin K <kbknapp@gmail.com>
2020-12-08 15:21:45 -0500

    adds vscode RPM to opensuse

---
+++ src/opensuse/code.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+function f_vscode_main() {
+    f_out "Installing VSCode"
+
+    sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+    sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/zypp/repos.d/vscode.repo'
+    sudo zypper refresh
+    sudo zypper install -y code
+}
