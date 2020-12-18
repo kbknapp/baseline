ede5d7a861f9582aef9183d346e9fa5bed553874
Kevin K <kbknapp@gmail.com>
2020-12-17 21:53:50 -0500

    adds missing dep for neovim plugin

--- src/fedora/neovim.sh
+++ src/fedora/neovim.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 function f_neovim_main() {
-    f_check_prog "curl"
-
     f_out "Installing NeoVIM"
 
-    sudo dnf install -y neovim python3-neovim fzf ctags
+    sudo dnf install -y neovim python3-neovim fzf ctags npm nodejs
 
     f_neovim_common
 }
