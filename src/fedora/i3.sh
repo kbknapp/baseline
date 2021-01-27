48325084859b45ccd4057ba3a6368e9d20d21a40
Kevin K <kbknapp@gmail.com>
2021-01-27 10:41:05 -0500

    adds new i3 deps

--- src/fedora/i3.sh
+++ src/fedora/i3.sh
@@ -7,12 +7,13 @@ function f_i3_pre() {
         jsoncpp-devel alsa-lib-devel wireless-tools-devel libmpdclient-devel \
         libcurl-devel cairo-devel xcb-proto xcb-util-devel xcb-util-wm-devel \
         xcb-util-image-devel libxcb-devel xcb-util-keysyms-devel xcb-util-xrm-devel \
-	yajl-devel startup-notification-devel libev-devel xcb-util-cursor-devel \
+	      yajl-devel startup-notification-devel libev-devel xcb-util-cursor-devel \
         libXinerama-devel libxkbcommon-devel libxkbcommon-x11-devel pcre-devel \
         pango-devel uthash-devel libX11-devel libXcomposite-devel libXfixes-devel \
         libXdamage-devel libXrender-devel libXext-devel libXrandr-devel \
         libconfig-devel libdrm-devel mesa-libGL-devel dbus-devel asciidoc \
-	desktop-file-utils libxdg-basedir-devel fontconfig-devel freetype-devel meson
+	      desktop-file-utils libxdg-basedir-devel fontconfig-devel freetype-devel meson \
+        perl-ExtUtils-MakeMaker
 
     f_out "Installing i3 Required Programs"
 
