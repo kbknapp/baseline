8dace7cb9bb1606dceda98137f759110701e5c85
Kevin K <kbknapp@gmail.com>
2021-01-27 10:41:34 -0500

    fixes standardnotes downloading

--- src/common/standard_notes.sh
+++ src/common/standard_notes.sh
@@ -2,12 +2,15 @@
 
 function f_standardnotes_main() {
     f_check_prog "wget"
+    local STANDARD_NOTES_LATEST
 
     f_out "Setting up standardnotes"
 
+    STANDARD_NOTES_LATEST=$(wget -q https://github.com/standardnotes/desktop/releases -O - | grep \"standard-notes-.*-x86_64.AppImage\" | head -n 1 | cut -d '\"' -f 2)
+
     # Download
     cd ~/.local/bin/ || exit
-    wget -c https://github.com/$(wget -q https://github.com/standardnotes/desktop/releases -O - | grep "standard-notes-.*-x86_64.AppImage" | head -n 1 | cut -d '"' -f 2)
-    chmod +x $(ls standard-notes-*.AppImage)
+    wget -O ~/.local/bin/$(echo $STANDARD_NOTES_LATEST | grep -oP '[^/]*$') -c https://github.com/${STANDARD_NOTES_LATEST}
+    chmod +x "$STANDARD_NOTES_LATEST"
     cd || exit
 }
