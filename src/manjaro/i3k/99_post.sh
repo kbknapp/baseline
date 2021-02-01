7049cb7808fd23ea5262dc8f2f012d01b3617a58
Kevin K <kbknapp@gmail.com>
2021-02-01 10:35:59 -0500

    fixes manjaro i3 KDE config copying

--- src/manjaro/i3k/99_post.sh
+++ src/manjaro/i3k/99_post.sh
@@ -4,7 +4,8 @@ function f_de_post() {
     f_out "Finalizing i3 (KDE) Setup"
 
     sudo cp ~/.dotfiles/i3/common/usr/bin/* /usr/bin
-    cp -r ~/.dotfiles/i3/common/home/.local/* ~/.local/
+    cp -r ~/.dotfiles/i3/common/home/.local/bin/* ~/.local/bin/
+    cp -r ~/.dotfiles/i3/common/home/.local/share/* ~/.local/share/
     cp -r ~/.dotfiles/i3/common/home/.config/fontconfig/ ~/.config/
     cp -r ~/.dotfiles/i3/common/home/.xprofile ~/
     cp -r ~/.dotfiles/i3/common/home/.Xresources ~/
