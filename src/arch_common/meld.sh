1c6abd83e6ebb0a02154f84e53c339a4afff6aa9
Kevin K <kbknapp@gmail.com>
2021-02-01 12:17:45 -0500

    wip: adding meld and vivaldi components

---
+++ src/arch_common/meld.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+function f_meld_main() {
+	f_out "Installing Meld"
+
+	$INSTALL_CMD meld
+}
---
+++ src/arch_common/vivaldi.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+function f_vivaldi_main() {
+	f_out "Installing Vivaldi"
+
+	$INSTALL_CMD vivalid vivalid-ffmpeg-codecs
+}
