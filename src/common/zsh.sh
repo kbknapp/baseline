96cea936a08059cc256db5e660d6ffe2e10e156d
Kevin K <kbknapp@gmail.com>
2021-02-24 22:20:43 -0500

    adds zsh-syntax-highlighting

--- src/common/zsh.sh
+++ src/common/zsh.sh
@@ -8,6 +8,7 @@ function f_zsh_config_main() {
     git clone https://github.com/momo-lab/zsh-abbrev-alias ~/.config/zsh-abbrev-alias/ || true
     git clone https://github.com/robbyrussell/oh-my-zsh ~/.oh-my-zsh || true
     git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions || true
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting || true
 
     cp ~/.dotfiles/zsh/zshrc-linux ~/.zshrc
     #cp ~/.dotfiles/p10k/.p10k.zsh ~/
