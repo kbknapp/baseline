02bd05c2f813d23a458ff2c0f8f6dd5eaf923203
Kevin K <kbknapp@gmail.com>
2021-05-29 21:54:20 -0400

    sorts rust tools and adds a few new ones

--- src/common/rust.sh
+++ src/common/rust.sh
@@ -34,121 +34,126 @@ function f_rust_apps_main() {
     clear
 
     _CARGO_PLUGINS=$(dialog --checklist "Which cargo plugins?" 400 400 19 \
-	  "cargo-outdated" "Display out of date dependencies" on \
-	  "cargo-tree" "" on \
-	  "cargo-about" "more info about deps" on \
-	  "cargo-modules" "" off \
-	  "cargo-cache" "" on \
-	  "cargo-crev" "Dependency Audit and Review" on \
-	  "cargo-graph" "Generate dependency graphs" off \
-	  "cargo-deps" "Generate dependency graphs" off \
-	  "cargo-edit" "Edit dependencies from the CLI" on \
-	  "cargo-geiger" "Unsafe statistics about deps" on \
-	  "cargo-bloat" "" on \
-	  "cargo-udeps" "Find unused deps" on \
-	  "cargo-thanks" "Star your deps" off \
-	  "cargo-audit" "Security Notifications" on \
-	  "cargo-update" "Update cargo-install'ed binaries" on \
-	  "cargo-expand" "Expand macros" on \
-	  "cargo-deny" "Dep graph lints" on \
-	  "cargo-license" "" on \
-	  "cargo-lichking" "Display license info for deps" on \
-	  "cargo-count" "Count lines of code" off \
-	  "cargo-sweep" "Clean out old build artifacts" off \
+	  "cargo-about"      "more info about deps" on \
+	  "cargo-audit"      "Security Notifications" on \
+	  "cargo-bloat"      "" on \
+	  "cargo-cache"      "" on \
 	  "cargo-call-stack" "Generate Call Graphs" on \
+	  "cargo-count"      "Count lines of code" off \
+	  "cargo-crev"       "Dependency Audit and Review" on \
+	  "cargo-deny"       "Dep graph lints" on \
+	  "cargo-deps"       "Generate dependency graphs" off \
+	  "cargo-edit"       "Edit dependencies from the CLI" on \
+	  "cargo-expand"     "Expand macros" on \
+	  "cargo-geiger"     "Unsafe statistics about deps" on \
+	  "cargo-graph"      "Generate dependency graphs" off \
+	  "cargo-license"    "" on \
+	  "cargo-lichking"   "Display license info for deps" on \
+	  "cargo-modules"    "" off \
+	  "cargo-outdated"   "Display out of date dependencies" on \
+	  "cargo-sweep"      "Clean out old build artifacts" off \
+	  "cargo-thanks"     "Star your deps" off \
+	  "cargo-tree"       "" on \
+	  "cargo-udeps"      "Find unused deps" on \
+	  "cargo-update"     "Update cargo-install'ed binaries" on \
         --output-fd 1)
     clear
 
     _RUST_DEV_TOOLS=$(dialog --checklist "Which Rust Dev Tools?" 400 400 15 \
-	  "amber" "Code Search and Replace" on \
-	  "clog-cli" "Change Log" on \
-	  "racer" "Rust completion support" on \
-	  "rusty-tags" "tags support" on \
-	  "just" "modern make" on \
-	  "gitui" "A TUI for git" on \
+	  "amber"      "Code Search and Replace" on \
+	  "clog-cli"   "Change Log" on \
 	  "git-absorb" "Auto fixup and rebase in git" on \
-	  "tokei" "Fast Line Count" on \
-	  "watchexec" "Run command on file change" on \
+	  "gitui"      "A TUI for git" on \
+	  "just"       "modern make" on \
+	  "racer"      "Rust completion support" on \
+	  "rusty-tags" "tags support" on \
+	  "tokei"      "Fast Line Count" on \
+	  "watchexec"  "Run command on file change" on \
         --output-fd 1)
     clear
 
     _RUST_CORE_TOOLS=$(dialog --checklist "Which Rust core tools?" 400 400 15 \
+	  "bat"      "pretty cat" on \
+	  "choose"   "An easier cut" on \
+	  "cw"       "wc clone" on \
+	  "diffr"    "diff clone" on \
 	  "exa"      "fancy ls" on \
 	  "fd-find"  "modern find" on \
-	  "ripgrep"  "modern grep" on \
-	  "cw"       "wc clone" on \
-	  "bat"      "pretty cat" on \
 	  "kmon"     "Kernel Modules" on \
 	  "lsd"      "fancy ls" on \
-	  "diffr"    "diff clone" on \
-	  "sd"       "sed clone" on \
 	  "procs"    "ps clone" on \
-	  "xcp"      "cp clone" on \
+	  "rargs"    "xargs and awk type tool" on \
+	  "ripgrep"  "modern grep" on \
 	  "rm-improved" "" on   \
+	  "runiq"    "uniq clone" on \
+	  "sd"       "sed clone" on \
 	  "tree-rs"  "tree clone " on \
-	  "rargs" "xargs and awk type tool" on \
-	  "runiq" "uniq clone" on \
+	  "xcp"      "cp clone" on \
+	  "zoxide"   "autojump tool, better cd" on \
         --output-fd 1)
     clear
 
     _RUST_DISK_TOOLS=$(dialog --checklist "Which Rust disk tools?" 400 400 15 \
-	  "du-dust" "du clone" on \
-	  "diskus"  "Disk Usage info" on \
-	  "dutree"  "du clone" on \
-	  "tin-summer" "sn" on \
-	  "dua-cli" "" on \
 	  "dirstat-rs" "" on \
+	  "diskus"     "Disk Usage info" on \
+	  "du-dust"    "du clone" on \
+	  "dua-cli"    "" on \
+	  "dutree"     "du clone" on \
+	  "tin-summer" "sn: find large files" on \
         --output-fd 1)
     clear
 
     _RUST_MISC_TOOLS=$(dialog --checklist "Which Rust misc tools?" 400 400 25 \
-	  "hyperfine"      "Benchmarking" on \
-	  "skim"           "fzf clone" on \
-	  "mdbook"         "write books in MD" on \
-	  "toast"          "Build using Docker" on \
-	  "websocket"      "Build websockets" on \
-	  "starship"       "shell prompt" off \
-	  "mdcat"          "cat for markdown" on \
-	  "topgrade"       "upgrade everything" on \
-	  "handlr"         "xdg-utils alternative" on \
-	  "bingrep"        "" on \
-	  "strace-analyzer" "" on \
-	  "mprober"        "Kernel prob utility" on \
-	  "battop"         "Battery Top" on \
-	  "gptman"         "" on \
-	  "broot"          "interactive tree" on \
-	  "dupe-krill"    "file deduplicator" on \
-	  "xcompress"      "Compression utility" on \
-	  "ruplacer"       "find and replace" on \
-	  "fastmod"        "find and replace" on \
-	  "ttdl"           "todo list" on \
-	  "genact"         "activity generator" off \
-	  "grex"           "regex builder" on \
 	  "bandwhich"      "bandwidth monitor" on \
-	  "pastel"         "Get color info in your terminal" on \
+	  "battop"         "Battery Top" on \
+	  "bingrep"        "" on \
+	  "broot"          "interactive tree" on \
+	  "dupe-krill"     "file deduplicator" on \
+	  "fastmod"        "find and replace" on \
+	  "genact"         "activity generator" off \
+	  "gptman"         "" on \
+	  "grex"           "regex builder" on \
+	  "handlr"         "xdg-utils alternative" on \
+	  "hyperfine"      "Benchmarking" on \
+	  "mdbook"         "write books in MD" on \
+	  "mdcat"          "cat for markdown" on \
 	  "miniserve"      "Mini HTTP server" on \
-	  "wasm-pack"      "" on \
+	  "mprober"        "Kernel prob utility" on \
+	  "pastel"         "Get color info in your terminal" on \
+	  "ruplacer"       "find and replace" on \
+	  "shellharden"    "Shell Script Lint and Fix" on \
+	  "skim"           "fzf clone" on \
+	  "starship"       "shell prompt" off \
+	  "strace-analyzer" "" on \
 	  "tealdeer"       "tldr" on \
+	  "toast"          "Build using Docker" on \
+	  "topgrade"       "upgrade everything" on \
+	  "ttdl"           "todo list" on \
+	  "wasm-pack"      "" on \
+	  "websocket"      "Build websockets" on \
+	  "xcompress"      "Compression utility" on \
+	  "zellij"         "tmux like terminal manager" on \
         --output-fd 1)
     clear
 
     _RUST_TOP_TOOLS=$(dialog --checklist "Which Rust top-like tools?" 400 400 25 \
-	  "bottom"         "top like tui" on \
+	  "bottom"  "top like tui" on \
+	  "ytop"    "top like tui" off \
         --output-fd 1)
     clear
 
     _RUST_DATA_TOOLS=$(dialog --checklist "Which Rust data tools?" 400 400 25 \
-	  "jql"            "json handling" on \
-	  "xsv"            "csv handling" on \
-	  "hexyl"          "Hex Viewer" on \
-	  "hx"             "Hex Viewer" on \
+	  "hexyl"   "Hex Viewer" on \
+	  "hx"      "Hex Viewer" on \
+	  "jql"     "json handling" on \
+	  "xsv"     "csv handling" on \
         --output-fd 1)
     clear
 
     _RUST_TOOLS_GIT=$(dialog --checklist "Which Rust from git repos?" 400 400 25 \
-	  "cjbassi/ytop"       "top like TUI" on \
 	  "denisidoro/navi"    "command search" on \
 	  "getzola/zola"       "static site generator" on \
+	  "tonarino/innernet"  "Wireguard manager" on \
         --output-fd 1)
     clear
 
@@ -182,5 +187,3 @@ function f_rust_main {
     rustup default nightly
     rustup component add rust-src
 }
-
-
