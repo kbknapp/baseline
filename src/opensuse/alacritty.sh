8850b7a15c9cb7c227ca7fbf5368b6c08dfd3399
Kevin K <kbknapp@gmail.com>
2021-08-05 13:28:35 -0400

    fixes function name typo

--- src/opensuse/alacritty.sh
+++ src/opensuse/alacritty.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-function f_alacritty_pre() {
+function _f_alacritty_pre() {
     f_out "Installing Alacritty deps"
 
     $INSTALL_CMD fontconfig-devel freetype-devel libxcb-devel \
         xcb-util-image-devel libXrender-devel
 }
+
 function f_alacritty_pre() {
     _f_alacritty_pre
 }
+
 function f_alacritty_ligatures_pre() {
     _f_alacritty_pre
 }
