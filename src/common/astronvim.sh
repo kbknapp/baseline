41881dfca6718b7dff6ede9144a5f43435d91e1b
Kevin K <kbknapp@gmail.com>
2023-06-06 11:42:16 -0400

    fix astronvim user config install

--- src/common/astronvim.sh
+++ src/common/astronvim.sh
@@ -6,6 +6,5 @@ function f_astronvim_main() {
   git clone --depth 1 https://github.com/AstroNvim/AstroNvim ~/.config/nvim
 
   nvim  --headless -c 'quitall'
-  git clone https://github.com/AstroNvim/user_example ~/.config/nvim/lua/user
-  cp ~/.dotfiles/astronvim/community.lua ~/.config/nvim/lua/user/community.lua
+  cp -r ~/.dotfiles/astronvim/* ~/.config/nvim/lua/user/
 }
